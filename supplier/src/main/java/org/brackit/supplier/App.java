b3706c0b04875f1b55742f1cc1ebcd106e51e270
vgrachov <vladimir.grachov@gmail.com>
2012-10-06 22:27:56 +0200

    first commit
    Change-Id: I0000000000000000000000000000000000000000

---
+++ supplier/src/main/java/org/brackit/supplier/App.java
@@ -0,0 +1,13 @@
+package org.brackit.supplier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
